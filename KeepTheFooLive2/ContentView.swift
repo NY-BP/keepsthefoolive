d68db758f21055a279060b904caa767051f0d0a8
No <no>
2020-03-18 23:24:56 +0100

    hello world app

---
+++ KeepTheFooLive2/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  KeepTheFooLive2
+//
+//  Created by Tamas on 2020. 03. 18..
+//  Copyright © 2020. -. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World, Im here!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
